37e4a7dd83a44b79e88e7020f3d74fae2bbce368
Alexander <37180898+MrSago@users.noreply.github.com>
2021-03-08 13:28:37 +0300

    replace space to tabs

--- Chapter 1/Section 1.2/beads/beads.cpp
+++ Chapter 1/Section 1.2/beads/beads.cpp
@@ -10,37 +10,37 @@ LANG: C++
 using namespace std;
 
 int main() {
-    ifstream fin("beads.in");
-    ofstream fout("beads.out");
+	ifstream fin("beads.in");
+	ofstream fout("beads.out");
 
-    int N; string str;
-    fin >> N >> str;
+	int N; string str;
+	fin >> N >> str;
 
-    int res = 0;
-    str = str + str;
-    for (int i = 0; i < N; ++i) {
-        char c = str[i];
-        int count = 0;
-        int replaces;
+	int res = 0;
+	str = str + str;
+	for (int i = 0; i < N; ++i) {
+		char c = str[i];
+		int count = 0;
+		int replaces;
 
-        if (c == 'w') {
-            replaces = 2;
-        } else {
-            replaces = 1;
-        }
+		if (c == 'w') {
+			replaces = 2;
+		} else {
+			replaces = 1;
+		}
 
-        int k = i;
-        for (int j = 0; j <= replaces; ++j) {
-            for (; k < N + i && (str[k] == c || str[k] == 'w'); ++k) {
-                count += 1;
-            }
-            c = str[k];
-        }
+		int k = i;
+		for (int j = 0; j <= replaces; ++j) {
+			for (; k < N + i && (str[k] == c || str[k] == 'w'); ++k) {
+				count += 1;
+			}
+			c = str[k];
+		}
 
-        res = max(res, count);
-    }
+		res = max(res, count);
+	}
 
-    fout << min(res, N) << '\n';
+	fout << min(res, N) << '\n';
 
 	return 0;
 }

412cf5842ea716d9c1099488588817f1a11f63b9
Alexander <37180898+MrSago@users.noreply.github.com>
2021-03-15 23:46:44 +0300

    Create wormhole.cpp

---
+++ Chapter 1/Section 1.4/wormhole/wormhole.cpp
@@ -0,0 +1,20 @@
+
+/*
+ID: sagosag1
+TASK: wormhole
+LANG: C++
+*/
+
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	ifstream fin("wormhole.in");
+	ofstream fout("wormhole.out");
+
+	
+
+	return 0;
+}
+

ebd73e8981b0e56ee600a8fa369ab5be11acf4c7
MrSago <sagosago16@gmail.com>
2021-03-16 13:07:17 +0300

    nu i che delat?

--- Chapter 1/Section 1.4/wormhole/sagosag1.cpp
+++ Chapter 1/Section 1.4/wormhole/sagosag1.cpp
@@ -7,13 +7,32 @@ LANG: C++
 
 #include <fstream>
 
+#define MAX_N (12)
+
 using namespace std;
+
+struct coord {
+    int x, y;
+};
+struct edge {
+    int v;
+    coord crd;
+};
+using graph = edge[MAX_N];
+
+int N;
+coord wormholes[MAX_N];
 
 int main() {
     ifstream fin("wormhole.in");
     ofstream fout("wormhole.out");
 
-    
+    fin >> N;
+    for (int i = 0; i < N; ++i) {
+        fin >> wormholes[i].x >> wormholes[i].y;
+    }
+
+
 
     return 0;
 }

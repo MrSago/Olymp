b421bce8387cb228c5de965c41e53e3ed8f9198f
Alexander <37180898+MrSago@users.noreply.github.com>
2021-03-18 06:49:56 +0300

    prepare files

---
+++ Chapter 1/Section 1.5/milk3/sagosag1_milk3.cpp
@@ -0,0 +1,20 @@
+
+/*
+ID: sagosag1
+TASK: milk3
+LANG: C++
+*/
+
+#include <fstream>
+
+using namespace std;
+
+int main() {
+    ifstream fin("milk3.in");
+    ofstream fout("milk3.out");
+
+
+
+    return 0;
+}
+

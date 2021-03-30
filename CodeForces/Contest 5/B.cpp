95ddeede0eb81d69ae734754a8ac8569bfb45c50
MrSago <sagosago16@gmail.com>
2021-03-30 13:17:14 +0300

    init CodeForces dir

---
+++ CodeForces/Contest 5/B.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+inline bool sost(int n) {
+    for (int i = 2; i <= sqrt(n); ++i) {
+        if (n % i == 0) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int N; cin >> N;
+
+    int n1 = N / 2, n2 = N - n1;
+    while (1) {
+        if (sost(n1) && sost(n2)) {
+            cout << n1 << ' ' << n2 << '\n';
+            return 0;
+        }
+        --n1; ++n2;
+    }
+
+    return 0;
+}

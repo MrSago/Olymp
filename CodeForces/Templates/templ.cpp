4e21347a365258c7d224b5153f4601791dcc7b58
Alexander <37180898+MrSago@users.noreply.github.com>
2021-03-30 15:09:09 +0300

    fix template

--- CodeForces/Templates/templ.cpp
+++ CodeForces/Templates/templ.cpp
@@ -1,18 +1,14 @@
 
 #include <iostream>
-#include <cmath>
 
 using namespace std;
-
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
 
-    vector<int> v(100);
-    int N; cin >> N;
 
-    
 
     return 0;
 }
+

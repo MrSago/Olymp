fa814b2f1ad387399f7ccec7c11a9d07a10517d9
Alexander <37180898+MrSago@users.noreply.github.com>
2021-04-17 09:42:37 +0300

    zadacha B

--- CodeForces/Susu/Contest #4 14.12.19/B.cpp
+++ CodeForces/Susu/Contest #4 14.12.19/B.cpp
@@ -1,6 +1,5 @@
 
 #include <iostream>
-#include <cmath>
 
 using namespace std;
 
@@ -9,9 +8,20 @@ int main() {
     cin.tie(0);
 
     int N; cin >> N;
+    int* F[3];for(int i=0;i<3;++i)F[i]=new int[N + 1];
 
+    F[0][0] = F[1][0] = F[2][0] = 0;
+    F[0][1] = F[1][1] = F[2][1] = 1;
 
+    for (int i = 2; i < N + 1; ++i) {
+        F[0][i] = F[0][i - 1] + F[1][i - 1] + F[2][i - 1];
+        F[1][i] = F[0][i - 1] + F[2][i - 1];
+        F[2][i] = F[0][i - 1] + F[1][i - 1] + F[2][i - 1];
+    }
 
+    cout << F[0][N] + F[1][N] + F[2][N] << '\n';
+
+    for(int i=0;i<3;++i)delete[]F[i];
     return 0;
 }
 

8d87a2eaf47a8ec0a94b5661750f1745e1b2b411
Alexander <37180898+MrSago@users.noreply.github.com>
2021-04-18 17:29:52 +0300

    milk3

--- Usaco-Tasks/Chapter 1/Section 1.5/milk3/sagosag1_milk3.cpp
+++ Usaco-Tasks/Chapter 1/Section 1.5/milk3/sagosag1_milk3.cpp
@@ -9,11 +9,52 @@ LANG: C++
 
 using namespace std;
 
+int A, B, C;
+bool pos[21][21] = { false };
+
+inline void rec(int c = C, int b = 0) {
+    if (pos[c][b]) {
+        return;
+    }
+    pos[c][b] = true;
+    int a = C - b - c;
+
+    if (c + b > B) rec(c - (B - b), B);
+    else           rec(0, c + b);
+
+    if (c + a > A) rec(c - (A - a), b);
+    else           rec(0, b);
+
+    if (b + c > C) rec(C, b - (C - c));
+    else           rec(b + c, 0);
+
+    if (b + a > A) rec(c, b - (A - a));
+    else           rec(c, 0);
+
+    if (a + c > C) rec(C, b);
+    else           rec(a + c, b);
+
+    if (a + b > B) rec(c, B);
+    else           rec(c, a + b);
+}
+
 int main() {
     ifstream fin("milk3.in");
     ofstream fout("milk3.out");
 
+    fin >> A >> B >> C;
 
+    rec();
+
+    for (int i = 0; i <= C; ++i) {
+        if (pos[i][C - i]) {
+            if (i != C - B && i != 0) {
+                fout << ' ';
+            }
+            fout << i;
+        }
+    }
+    fout << '\n';
 
     return 0;
 }

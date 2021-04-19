b9b68aed5bf251d376f6e12d2621d2b2e0fdd4c3
Alexander <37180898+MrSago@users.noreply.github.com>
2021-04-19 20:22:15 +0300

    numtri

---
+++ Usaco-Tasks/Chapter 1/Section 1.6/numtri/sagosag1_numtri.cpp
@@ -0,0 +1,38 @@
+
+/*
+ID: sagosag1
+TASK: numtri
+LANG: C++
+*/
+
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ifstream fin("numtri.in");
+    ofstream fout("numtri.out");
+
+    int R; fin >> R;
+
+    vector<vector<int>> v(R);
+    
+    for (int i = 0; i < R; ++i) {
+        v[i].resize(i + 1);
+        for (int j = 0; j <= i; ++j) {
+            fin >> v[i][j];
+        }
+    }
+
+    for (int i = R - 2; i >= 0; --i) {
+        for (int j = 0; j <= i; ++j) {
+            v[i][j] += max(v[i + 1][j], v[i + 1][j + 1]);
+        }
+    }
+
+    fout << v[0][0] << '\n';
+
+    return 0;
+}
+

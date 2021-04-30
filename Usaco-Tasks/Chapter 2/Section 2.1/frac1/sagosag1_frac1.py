cb8b02cd685e6ccf12def38b5cef3b9d672f4582
Alexander <37180898+MrSago@users.noreply.github.com>
2021-04-30 22:04:12 +0300

    full solution on python

--- Usaco-Tasks/Chapter 2/Section 2.1/frac1/sagosag1_frac1.py
+++ Usaco-Tasks/Chapter 2/Section 2.1/frac1/sagosag1_frac1.py
@@ -8,15 +8,14 @@ LANG: PYTHON3
 from fractions import Fraction
 
 with open("frac1.in", "r") as fin:
-    N = int(fin.readline().strip())
+    N = int(fin.readline())
 
-res = []
+s = set()
 for i in range(N, 1, -1):
     for j in range(1, i):
-        f = Fraction(j, i)
-        if f not in res:
-            res.append(f)
-res.sort()
+        s.add(Fraction(j, i))
+
+res = sorted(s)
 
 with open("frac1.out", "w") as fout:
     fout.write("0/1\n")

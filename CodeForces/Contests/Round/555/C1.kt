4b60104e407a4afa02e677bb4bb091005324c9cc
Alexander <37180898+MrSago@users.noreply.github.com>
2021-06-27 20:55:29 +0300

    C1 + C2

---
+++ CodeForces/Contests/Round/555/C1.kt
@@ -0,0 +1,91 @@
+
+private fun readln() = readLine()!!
+private fun readlnByte() = readln().toByte()
+private fun readlnShort() = readln().toShort()
+private fun readlnInt() = readln().toInt()
+private fun readlnLong() = readln().toLong()
+private fun readlnFloat() = readln().toFloat()
+private fun readlnDouble() = readln().toDouble()
+private fun readlnBigInt(radix: Int = 10) = readln().toBigInteger(radix)
+private fun readlnBigDecimal() = readln().toBigDecimal()
+
+private fun lineSequence(limit: Int = Int.MAX_VALUE) = generateSequence { readLine() }.constrainOnce().take(limit)
+private fun readlnStrings() = readln().split(' ')
+private fun readlnBytes() = readlnStrings().map { it.toByte() }
+private fun readlnShorts() = readlnStrings().map { it.toShort() }
+private fun readlnInts() = readlnStrings().map { it.toInt() }
+private fun readlnLongs() = readlnStrings().map { it.toLong() }
+private fun readlnFloats() = readlnStrings().map { it.toFloat() }
+private fun readlnDoubles() = readlnStrings().map { it.toDouble() }
+
+private fun readByteArray() = readlnStrings().run { ByteArray(size) { get(it).toByte() } }
+private fun readShortArray() = readlnStrings().run { ShortArray(size) { get(it).toShort() } }
+private fun readIntArray() = readlnStrings().run { IntArray(size) { get(it).toInt() } }
+private fun readLongArray() = readlnStrings().run { LongArray(size) { get(it).toLong() } }
+private fun readFloatArray() = readlnStrings().run { FloatArray(size) { get(it).toFloat() } }
+private fun readDoubleArray() = readlnStrings().run { DoubleArray(size) { get(it).toDouble() } }
+
+private fun readlnByteArray(n: Int) = ByteArray(n) { readlnByte() }
+private fun readlnShortArray(n: Int) = ShortArray(n) { readlnShort() }
+private fun readlnIntArray(n: Int) = IntArray(n) { readlnInt() }
+private fun readlnLongArray(n: Int) = LongArray(n) { readlnLong() }
+private fun readlnFloatArray(n: Int) = FloatArray(n) { readlnFloat() }
+private fun readlnDoubleArray(n: Int) = DoubleArray(n) { readlnDouble() }
+
+private fun readByteArray2d(rows: Int, cols: Int) = Array(rows) { readByteArray().also { require(it.size == cols) } }
+private fun readShortArray2d(rows: Int, cols: Int) = Array(rows) { readShortArray().also { require(it.size == cols) } }
+private fun readLongArray2d(rows: Int, cols: Int) = Array(rows) { readLongArray().also { require(it.size == cols) } }
+private fun readIntArray2d(rows: Int, cols: Int) = Array(rows) { readIntArray().also { require(it.size == cols) } }
+private fun readFloatArray2d(rows: Int, cols: Int) = Array(rows) { readFloatArray().also { require(it.size == cols) } }
+private fun readDoubleArray2d(rows: Int, cols: Int) = Array(rows) { readDoubleArray().also { require(it.size == cols) } }
+
+private fun isWhiteSpace(c: Char) = c in " \r\n\t"
+
+
+
+
+fun main() {
+    val n = readlnInt()
+    val a = readlnInts()
+
+    val res = CharArray(n)
+    var count = 0
+    var l = 0
+    var r = n - 1
+    var last = -1
+
+    while (l <= r) {
+        var okLeft = false
+        var okRight = false
+        if (last < a[l]) {
+            okLeft = true
+        }
+        if (last < a[r]) {
+            okRight = true
+        }
+        if (!okLeft && !okRight) {
+            break
+        }
+        if (okLeft && okRight) {
+            if (a[l] <= a[r]) {
+                last = a[l++]
+                res[count++] = 'L'
+            } else {
+                last = a[r--]
+                res[count++] = 'R'
+            }
+            continue
+        }
+        if (okLeft) {
+            last = a[l++]
+            res[count++] = 'L'
+        } else {
+            last = a[r--]
+            res[count++] = 'R'
+        }
+    }
+
+    println(count)
+    println(res.slice(0 until count).joinToString(""))
+}
+

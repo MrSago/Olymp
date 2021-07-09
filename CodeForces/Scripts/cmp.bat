0f845ac23e4f3828e057b3045ee0749ac4b9dc9e
Alexander <37180898+MrSago@users.noreply.github.com>
2021-07-09 21:43:32 +0300

    fix cmp script

--- CodeForces/Scripts/cmp.bat
+++ CodeForces/Scripts/cmp.bat
@@ -3,10 +3,15 @@
 
 g++ -O3 %1 -o %1.exe
 if not %errorlevel% == 0 (
-    echo Exit script
+    echo Exiting the script
     exit
 )
 
 et -f %1.exe -w 5000 <cf.in >cf.out
+if not %errorlevel% == 0 (
+    echo Exiting the script
+    exit
+)
+
 fc cf.out cf.ok
 

89d51be934e996d9de7b7353887661a63d418b83
Alexander <37180898+MrSago@users.noreply.github.com>
2021-07-10 23:54:56 +0300

    fix

--- CodeForces/Scripts/cmp.bat
+++ CodeForces/Scripts/cmp.bat
@@ -1,13 +1,13 @@
 
 @echo off
 
-g++ -O3 %1 -o %1.exe
+g++ -O3 %1 -o %~n1.exe
 if not %errorlevel% == 0 (
     echo Exiting the script
     exit
 )
 
-et -f %1.exe -w 5000 <cf.in >cf.out
+et -f %~n1.exe -w 5000 <cf.in >cf.out
 if not %errorlevel% == 0 (
     echo Exiting the script
     exit

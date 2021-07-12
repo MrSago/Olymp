5fd33d0c7f7b0aa9591b25a8c281ff65d98e052c
Alexander <37180898+MrSago@users.noreply.github.com>
2021-07-12 22:43:11 +0300

    new templ

--- CodeForces/Templates/templ.cpp
+++ CodeForces/Templates/templ.cpp
@@ -1,22 +1,37 @@
 
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 using LL = long long;
 using ULL = unsigned long long;
+using LD = long double;
+
+using vi = vector<int>;
+using vll = vector<LL>;
+using vull = vector<ULL>;
+using vb = vector<bool>;
+
+using Graph = vector<vi>;
 
 inline int nxt() { int x; scanf("%d", &x); return x; }
 inline LL nxtLL() { LL x; scanf("%lld", &x); return x; }
 inline ULL nxtULL() { ULL x; scanf("%llu", &x); return x; }
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(0);
+constexpr int MOD = 1000007;
 
-    int tt = nxt();
-    while (tt--) {
-        
+
+void solve(int test = 1) {
+    
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    for (int t = 1, tc = nxt(); t <= tc; ++t) {
+        solve(t);
     }
 
     return 0;

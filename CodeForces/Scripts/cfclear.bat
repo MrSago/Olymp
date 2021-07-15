bed6d4a45bc70d0ea6678beb0a7db78451151625
Alexander <37180898+MrSago@users.noreply.github.com>
2021-07-15 11:47:04 +0300

    script for clear

---
+++ CodeForces/Scripts/cfclear.bat
@@ -0,0 +1,8 @@
+
+@echo off
+
+del /S C:\Users\sago0\source\repos\Olymp\CodeForces\Contests\*cf.in
+del /S C:\Users\sago0\source\repos\Olymp\CodeForces\Contests\*cf.out
+del /S C:\Users\sago0\source\repos\Olymp\CodeForces\Contests\*cf.ok
+del /S C:\Users\sago0\source\repos\Olymp\CodeForces\Contests\*.exe
+

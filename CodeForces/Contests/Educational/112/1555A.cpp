42849db91e9439945bc62acb83d56a136aa7e792
Alexander <37180898+MrSago@users.noreply.github.com>
2021-07-31 13:28:37 +0300

    1555

---
+++ CodeForces/Contests/Educational/112/1555A.cpp
@@ -0,0 +1,48 @@
+
+/*
+    author: MrS4g0
+    created: 30.07.2021 17:34:40
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl ('\n')
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+
+using vb = vector<bool>;
+using vc = vector<char>;
+using vi = vector<int>;
+using vll = vector<ll>;
+using vull = vector<ull>;
+
+using pii = pair<int,int>;
+using graph = vector<vi>;
+
+template <typename T = int>
+inline T nxt() { T x; cin >> x; return x; }
+
+constexpr int MOD = int(1e9) + 7;
+constexpr int NMOD = 998244353;
+
+
+void solve() {
+    ull n = max(nxt<ull>(), 6ull);
+    cout << (n += n & 1) / 2ull * 5ull << nl;
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    for (int t = 1, tc = nxt(); t <= tc; ++t) {
+        solve();
+    }
+
+    return 0;
+}
+

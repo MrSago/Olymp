b4a0c468a1b2dff0ed0c66dbdc77ddf5f6cce238
Alexander <37180898+MrSago@users.noreply.github.com>
2021-10-07 15:12:29 +0300

    segtree update

--- Practice/Data Structures/Segment tree/segtree.cpp
+++ Practice/Data Structures/Segment tree/segtree.cpp
@@ -10,14 +10,14 @@ static constexpr int MAXN = (int)1e5;
 vector<int> a;
 int n, tree[4 * MAXN];
 
-void build(int v, int l, int r) {
-    if (l == r) {
-        tree[v] = a[l];
+void build(int v, int tl, int tr) {
+    if (tl == tr) {
+        tree[v] = a[tl];
     } else {
-        int m = (l + r) >> 1;
-        build(v * 2, l, m);
-        build(v * 2 + 1, m + 1, r);
-        tree[v] = tree[v * 2] + tree[v * 2 + 1];
+        int tm = (tl + tr) >> 1;
+        build(v << 1, tl, tm);
+        build((v << 1) + 1, tm + 1, tr);
+        tree[v] = tree[v << 1] + tree[(v << 1) + 1];
     }
 }
 
@@ -28,9 +28,24 @@ int sum_tree(int v, int tl, int tr, int l, int r) {
     if (l == tl && r == tr) {
         return tree[v];
     }
-    int m = (tl + tr) >> 1;
-    return sum_tree (v * 2, tl, m, l, min(r, m))
-         + sum_tree (v * 2 + 1, m + 1, tr, max(l, m + 1), r);
+    int tm = (tl + tr) >> 1;
+    return sum_tree (v << 1, tl, tm, l, min(r, tm)) +
+           sum_tree ((v << 1) + 1, tm + 1, tr, max(l, tm + 1), r);
+}
+
+void update (int v, int tl, int tr, int pos, int new_val) {
+	if (tl == tr) {
+		tree[v] = new_val;
+    }
+	else {
+		int tm = (tl + tr) >> 1;
+		if (pos <= tm) {
+			update (v << 1, tl, tm, pos, new_val);
+        } else {
+			update ((v << 1) + 1, tm + 1, tr, pos, new_val);
+        }
+		tree[v] = tree[v << 1] + tree[(v << 1) + 1];
+	}
 }
 
 int main() {

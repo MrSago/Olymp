214fa27da8b07bcf03fcbaf821486aec7ded6b0d
Alexander <37180898+MrSago@users.noreply.github.com>
2022-04-21 19:39:54 +0300

    cf div4

---
+++ CodeForces/Round/784/C.cpp
@@ -0,0 +1,79 @@
+
+/*
+    author: MrS4g0
+    created: 21.04.2022 17:43:25
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl ('\n')
+
+using i64 = long long;
+using ui64 = unsigned long long;
+using ld = long double;
+
+using i128 = __int128;
+using ui128 = unsigned __int128;
+inline i128 iget() { i64 x; cin >> x; return static_cast<i128>(x); }
+inline ui128 uiget() { ui64 x; cin >> x; return static_cast<ui128>(x); }
+
+using vb = vector<bool>;
+using vc = vector<char>;
+using vi = vector<int>;
+using vi64 = vector<i64>;
+using vui64 = vector<ui64>;
+
+using pi = pair<int,int>;
+using pi64 = pair<i64,i64>;
+using pui64 = pair<ui64,ui64>;
+using graph = vector<vi>;
+
+template <typename T = int>
+inline T nxt() { T x; cin >> x; return x; }
+
+constexpr int MOD = int(1e9) + 7;
+constexpr int NMOD = 998244353;
+
+
+void solve() {
+    ui64 n = nxt<ui64>();
+
+    ui64 bm = 0;
+    for (ui64 i = 0, k = n; k--; ++i) {
+        ui64 v = nxt();
+        if (v & 1ULL) {
+            bm += 1ULL << i;
+        }
+    }
+
+    ui64 v1 = 0, v0 = 0;
+    for (ui64 i = 0; i < n; ++i) {
+        if (i & 1ULL) {
+            v1 += 1ULL << i;
+        } else {
+            v0 += 1ULL << i;
+        }
+    }
+
+    bitset<64> bs(bm);
+
+    if (!(bm ^ v1) || !(bm ^ v0) || bs.count() == 0 || bs.count() == n) {
+        cout << "YES\n";
+    } else {
+        cout << "NO\n";
+    }
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    for (int t = 1, tc = nxt(); t <= tc; ++t) {
+        solve();
+    }
+
+    return 0;
+}
+
